e6834f97124a287de8f6d70be9a2fcd2037ef653
Vinay Bijalwan <7480654+vinaybijalwan@users.noreply.github.com>
2024-02-08 15:18:57 +0530

    This commit for ternary_operator

---
+++ ternary_operator.js
@@ -0,0 +1,39 @@
+//Date: 08-02-2024
+
+
+// The ternary operator is a conditional operator which evaluates either
+//  of two expressions – a true expression and a false expression – 
+// based on a conditional expression that you provide.
+
+// condition ? trueExpression : falseExpression
+
+
+//understand with example 
+//  1. from if else constion 
+const age = 18;
+
+// if(age>18){
+//     console.log("You are Right for Vote")
+// }
+// else{
+//     console.log("You are Right for Vote")
+// }
+
+// out put 
+
+// You are Right for Vote
+
+//2.  now convert this if else condtion to ternary operator
+// syntx => condition ? trueExpression : falseExpression
+
+age>18 ? console.log("You are Right for Vote") : console.log("You are Right for Vote")
+
+
+// out put 
+
+// You are Right for Vote
+
+
+// using true false
+let a = true
+ a= true ? console.log("You are true" ) : console.log("You are not ture")
